26f886673135de07582ac4bed6d67815ca939bb5
mVenetos97 <53448901+mVenetos97@users.noreply.github.com>
2019-10-30 15:51:18 -0400

    Added line broadening

--- app/dimension/post_simulation_widgets.py
+++ app/dimension/post_simulation_widgets.py
@@ -1,4 +1,9 @@
 # -*- coding: utf-8 -*-
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app.custom_widgets import custom_input_group
 from app.custom_widgets import custom_slider
 
 
@@ -8,15 +13,39 @@ __email__ = ["venetos.5@buckeyemail.osu.edu"]
 
 def gaussian_linebroadening_widget(i):
     broaden_range = {0: "0", 2: "2", 4: "4", 6: "6", 8: "8", 10: "10"}
+
+    broadeningFunction = dcc.RadioItems(
+        options=[
+            {"label": "Lorentzian", "value": "Lorentz"},
+            {"label": "Gaussian", "value": "Gaussian"},
+        ],
+        value="Lorentz",
+        # labelStyle={'display': 'inline'}
+        id=f"Apodizing_function-{i}",
+    )
+
     line_broadening = custom_slider(
         label="Line Broadening",
-        return_function=lambda x: f"{x}  ppm",
+        return_function=lambda x: f"{x}",
+        # if broadeningFunction.value == 'Lorentz':
         min=0,
-        max=10,
-        step=1,
+        max=1000,
+        step=25,
+        # elif broadeningFunction.value == 'Gaussian':
+        # min=0,
+        # max=10,
+        # step=1,
         value=0,
-        marks=broaden_range,
+        # marks=broaden_range,
         id=f"broadening_points-{i}",
     )
 
+    # return [broadeningFunction, line_broadening]
     return [line_broadening]
+
+
+# @app.callback(
+#     dash.dependencies.Output('Apodization', 'children'),
+#     [dash.dependencies.Input('Apodizing_function-0', 'value')])
+# def Apodization_label(value):
+#     return value
--- app/methods/post_simulation_functions.py
+++ app/methods/post_simulation_functions.py
@@ -1,13 +1,56 @@
 # -*- coding: utf-8 -*-
-# from scipy.ndimage import gaussian_filter
-#
-#
-# def line_broadening(datum, sigma):
-#     # normalize = datum.max()
-#     return gaussian_filter(datum, sigma, 0)
-#     # return (datum / datum.max()) * normalize
-#
-#
-# if __name__ == "__main__":
-#     import numpy
-#     line_broadening(numpy.asarray([1, 2]), 2)
+import numpy as np
+from numpy.fft import fft
+from numpy.fft import fftshift
+from numpy.fft import ifft
+from numpy.fft import ifftshift
+
+# import csdmpy as
+
+
+def line_broadening(x, amp, sigma, broadType):
+    """
+    This function evaluates
+    ..math::
+
+    """
+
+    freq = x.coordinates.to("Hz")
+
+    TimeDomain = ifft(ifftshift(amp))
+    TimeDomain = np.roll(TimeDomain, int(x.count / 2))
+    t = np.arange(x.count) - int(x.count / 2)
+
+    time = t * 1 / (len(freq) * x.increment.to("Hz").value)
+
+    if broadType == "Lorentz" and sigma != 0:
+        broadSignal = np.exp(-sigma * np.abs(time))
+    elif broadType == "Gaussian" and sigma != 0:
+        broadSignal = np.exp(-(time / sigma) ** 2 / 2) / (np.sqrt(2 * np.pi) * sigma)
+    else:
+        broadSignal = 1
+
+    appodized = np.roll(TimeDomain * broadSignal, -int(x.count / 2))
+
+    return fftshift(fft(appodized))
+
+
+def post_simulation(function, csdm_object, **kwargs):
+    csdm_local = csdm_object.copy()
+
+    x = csdm_local.dimensions[0]
+    for datum in csdm_local.dependent_variables:
+        y = datum.components[0]
+        datum.components[0] = function(x, y, **kwargs)
+
+    return csdm_local
+    # return [
+
+    #         function(datum, **kwargs) for datum in local_data if not isinstance(datum, list)
+    # ]
+
+
+if __name__ == "__main__":
+    import numpy
+
+    line_broadening(numpy.asarray([1, 2]), 2)
