c541135a9364991b5e24af024e5949223b175a20
Deepansh Srivastava <deepansh2012@gmail.com>
2020-10-29 21:27:06 -0400

    added ga

--- app/app.py
+++ app/app.py
@@ -71,6 +71,15 @@ html_head = """
 <!DOCTYPE html>
 <html>
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-L6STWPJCHV"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-L6STWPJCHV');
+        </script>
         {%metas%}
         <title>{%title%}</title>
         {%favicon%}
