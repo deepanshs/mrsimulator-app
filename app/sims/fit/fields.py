6ad15a4e1304afd045adfa08233658ccec2d7d65
Matthew Giammar <mdgiammar@gmail.com>
2021-05-14 20:18:54 -0400

    fixed sorting for sys/mth over 10 elements

--- app/sims/fit/fields.py
+++ app/sims/fit/fields.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import json
-import operator
 from datetime import datetime
 
 import dash_bootstrap_components as dbc
@@ -223,16 +222,12 @@ def group_params(params_dict):
     keys = [
         "_".join(tup)
         for tup in sorted(
-            [key.split("_") for key in params_dict.keys()], key=operator.itemgetter(1)
+            [key.split("_") for key in params_dict.keys()], key=lambda x: int(x[1])
         )
     ]
 
-    print("Keys List", keys)
-
     if len(keys) == 0:
         return {}
-
-    # BUG: when i in PREFIX_i_... does not appear in order splitting not working always
 
     tmp_sys, tmp_mth = [], []
     index_sys, index_mth = 0, 0
@@ -260,7 +255,6 @@ def group_params(params_dict):
                     )
                 )
                 index_mth += 1
-                print("MTH", tmp_mth)
                 tmp_mth = []
             tmp_mth += [key]
 
@@ -270,7 +264,6 @@ def group_params(params_dict):
     mth_group.append(
         ({k: params_dict[k] for k in tmp_mth}, index_mth + 1, f"Method {index_mth}")
     )
-    print("MTH", tmp_mth)
     return sys_group, mth_group
 
 
